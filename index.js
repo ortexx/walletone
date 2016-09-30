6a7c14a4fe5a50f117d89fb668d35f208ece1e22
Ortex <mywebname@mail.ru>
2016-09-30 08:29:35 +0300

    init

---
+++ index.js
@@ -0,0 +1,169 @@
+"use strict";
+
+const iconv = require('iconv-lite');
+const crypto = require('crypto');
+const _ = require('lodash');
+
+class Wallet {
+    constructor(key, merchantId, defaults) {
+        if(typeof merchantId == "object") {
+            defaults = merchantId;
+            merchantId = undefined;
+        }
+        
+        this.key = key;        
+        this.defaults = defaults || {};
+        
+        if(merchantId) {
+            this.defaults.WMI_MERCHANT_ID = merchantId;
+        }
+    }
+    
+    getFormFields(data) {
+        data = _.merge(this.defaults, data)
+        delete data.WMI_SIGNATURE;
+        
+        let fields = [];
+        let values = '';
+        
+        let sortFn = (a, b) => {
+            a = a.toLowerCase();
+            b = b.toLowerCase();
+            
+            return a > b? 1: (a < b? -1: 0);
+        }
+        
+        Object.keys(data).sort(sortFn).map((name) => {            
+            let value = data[name];
+            
+            if(name == "WMI_DESCRIPTION" && !name.match(/^BASE64:/)) {
+                value = 'BASE64:' + new Buffer(value).toString('base64')
+            }
+            
+            if (Array.isArray(value)) {
+                values += value.sort(sortFn).join('');
+                
+                value.map((val) => { 
+                    fields.push({ name: name, value: val });
+                })
+            }
+            else {
+                values += value;
+                fields.push({ name: name, value: value });
+            }
+        });
+        
+        let signature = crypto.createHash('md5').update(iconv.encode(values + this.key, 'win1251')).digest('base64');
+        
+        fields.push({ name: "WMI_SIGNATURE", value: signature });
+        
+        return fields;
+    }
+    
+    getSignature(data) {
+        let fields = this.getFormFields(data);
+        
+        return fields[fields.length - 1].value;
+    }    
+    
+    checkSignature(data, signature) {
+        if(!signature) {
+            signature = data.WMI_SIGNATURE;
+        }
+        
+        if(!signature) {
+            return false;
+        }
+        
+        return this.getSignature(data) == signature;
+    }
+    
+    answer(meta) {
+        let message = '', answer = "WMI_RESULT=OK";         
+        
+        if(meta instanceof Error) {
+            answer = "WMI_RESULT=RETRY";
+            message = meta.message;
+        }
+        else if(meta) {
+            message = meta;
+        }
+        
+        if(message) {
+            message = encodeURIComponent(message);
+        }
+        
+        if(message) {
+            answer += `&WMI_DESCRIPTION=${message}`;
+        }
+        
+        return answer;
+    }
+    
+    getPaymentUrl() {
+        return 'https://wl.walletone.com/checkout/checkout/Index';
+    }
+    
+    notify(fn, onError) {
+        return (req, res) => {  
+            let data = req.body || {};
+            let err;
+            let meta;
+            
+            let ok = () => {
+                return res.send(this.answer());;
+            }
+            
+            let fail = (err, meta) => {                
+                if(onError) {
+                    onError(err, meta);
+                }
+                
+                return res.send(this.answer(err));
+            }
+            
+            if(!this.checkSignature(data)) {
+                err = new Error('Wrong signature');
+                meta = {
+                    reason: 'signature',
+                    signature: data.WMI_SIGNATURE
+                }
+            } 
+            else if(data.WMI_ORDER_STATE != "ACCEPTED") {
+                err = new Error('Order was not accepted');
+                meta = {
+                    reason: 'state',
+                    state: data.WMI_ORDER_STATE
+                }
+            } 
+            
+            if(err) {                
+                return fail(err, meta);
+            }
+                     
+            function callback(err) {
+                if(err) {
+                    return fail(err);
+                }
+                
+                return ok();
+            }
+            
+            if(!fn) {
+                return ok();
+            }
+            
+            let result = fn.call(this, req.body, callback);
+            
+            if(result && typeof result == 'object') {
+                result.then(() => {
+                    ok();
+                }).catch((err) => {
+                    fail(err);
+                })
+            }
+        }
+    }
+}
+
+module.exports = Wallet;
---
+++ readme.md
@@ -0,0 +1,101 @@
+# Install 
+`npm install walletone`
+
+# About
+Walletone payment module (walletone.com)
+
+# Example
+```js
+const W1 = require("walletone");
+
+let defaultData = {
+    WMI_SUCCESS_URL: 'http://example.com/success/',
+    WMI_FAIL_URL: 'http://example.com/fail/',,
+};
+
+let secretKey = "keykeykey";
+let merchantId = "000000001";
+
+const w1 = new W1(secretKey, merchantId, defaultData);
+
+// Create form data
+let fields = w1.getFormFields({
+    WMI_PAYMENT_AMOUNT: '10',
+    WMI_CURRENCY_ID: '643',
+    WMI_DESCRIPTION: 'Recharge',
+    WMI_CUSTOMER_EMAIL: 'user@example.com',
+    WMI_AUTO_LOCATION: "1"
+    // ...and other options
+});
+
+console.log(fields)  // return sorted fields and signature too
+/* output
+ [
+    { name: WMI_AUTO_LOCATION', value: '10' },
+    { name: WMI_CURRENCY_ID', value: '643' }     
+    ... 
+    { name: WMI_SIGNATURE', value: 'hashhashhash' }
+ ]
+*/
+```
+
+```html
+<form method="POST" action="{{ w1.getPaymentUrl() }}" accept-charset="UTF-8">
+    {% for field in fields %}
+    <input name="{{ field.name }}" type="hidden" value="{{ field.value }}"/>
+    {% endfor %}             
+    <button type="submit">Ok</button>
+</form>
+```
+
+```js
+// notify handler from walletone.com
+
+const express = require('express');
+const app = express();
+
+let successHandler = (data, callback) => {
+    // data == req.body    
+    // save payment info in db e.t.c    
+    // callback() or return promise
+};
+
+let errorHandler = (err, meta) => {
+    // you can save to a file, db e.t.c.
+    // operation must be synchronous or in the background 
+};
+
+app.post('payments/notify/handler/', w1.notify(successHandler, errorHandler));
+
+```
+
+# Description
+You can write custom notify handler, but library version includes data/signature validation and automatically send all headers in necessary format;
+
+# API
+### .constructor(secretKey, merchantId, defaultData)
+secretKey and merchantId you can find in your w1 account.  
+defaultData will be merged with other data in .getFormFields
+
+### .getFormFields(data)
+returns sorted data and signature in the array of objects
+
+### .getSignature(data)
+returns data signature;
+
+### .checkSignature(data, siganture) or .checkSignature(data) // data includes WMI_SIGANTURE
+checks the validity of the signature 
+
+### .answer(meta)
+returns answer in w1 format  
+meta type can be string or Error instance, if string then result is "OK" else "RETRY" 
+
+### .getPaymentUrl()
+returns w1 form action url
+
+### .notify(fn, onError)
+w1 notify handler, it is "connect" middleware.
+
+
+
+
